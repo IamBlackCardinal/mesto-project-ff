657cc2b07a3da7a588c186c08d4f470f394e93b9
Margarita Benidovskaia <ritabuzmakova@yandex.ru>
2023-11-26 20:22:30 +0300

    Добавлены функция добавлен

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/IamBlackCardinal/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,25 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+const container = document.querySelector('.content');
+const cardsContainer = container.querySelector('.places__list');
+
+function addCard(card, deleteFunction) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+
+  cardElement.querySelector('.card__image').src = card.link;
+  cardElement.querySelector('.card__image').alt = card.name;
+  cardElement.querySelector('.card__title').textContent = card.name;
+
+  deleteButton.addEventListener('click', () => deleteFunction(cardElement));
+
+  return cardElement;
+}
+
+function deleteCard(card) {
+  card.remove();
+}
+
+initialCards.forEach(card => cardsContainer.append(addCard(card, deleteCard)));
